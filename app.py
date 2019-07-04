4545054ec576077f8653e5f74779a7d144305c9d
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-07-04 12:01:50 +0200

    Filter by user id, when delete task

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 # configure flask_login
 login = LoginManager(app)
